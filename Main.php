ff6d288a059a2dc1e6c5c9bf9c39d0fce3765541
Ilya Ananyev <ilya.ananyev@chamaeleon.de>
2017-10-21 10:07:11 +0200

    added get request to blockchain.info

--- Main.php
+++ Main.php
@@ -10,6 +10,17 @@ class Main
 {
     public function __construct()
     {
-        
+        print $this->getData();
+    }
+
+    private function getData() {
+        $ch = curl_init("https://blockchain.info/ticker"); // such as http://example.com/example.xml
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
 }
+
+new Main();
